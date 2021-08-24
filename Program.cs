f55db9211ee75f122362af59949fa7d56c0fa7d0
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-08-24 15:32:36 -0500

    added code prompting user to guess a secret number and displays user's guest back to the console

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Let's Play A Guessing Game! Can you guess the secret number?");
+            // Console.Write("What is your guess? ");
+            // string guess = Console.ReadLine();
+            // Console.WriteLine($"Your guess is: {guess}");
+            Console.WriteLine(@"
+            Let's Play a Guessing Game! Can you guess the secret number?
+                _...._
+              .`      `.
+             / ***      \         
+            : **         :         
+            :            :                                        
+             \          /       
+              `-.,,,,.-'              
+               _(    )_
+              )        (
+             (          )
+              `-......-`
+            ");
+
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            Console.WriteLine($"Your guess is: {guess}");
         }
     }
 }
