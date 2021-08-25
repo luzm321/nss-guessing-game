6c30461fef95e5d3c59d169fe7b9d9307048e3fe
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-08-24 19:05:49 -0500

    added code that gives user four chances to guess the number with success/failure/out of guesses message displaying; phase 3 completed

--- Program.cs
+++ Program.cs
@@ -21,45 +21,75 @@ namespace GuessingGame
               `-......-`
             ");
 
-            Console.Write("What is your guess? ");
+            Console.Write("Enter your guess: ");
             string guess = Console.ReadLine();
-            int parsedGuess = int.Parse(guess);
+            int parsedGuess = int.Parse(guess); // converting string to int
             int secretNum = 21;
+            int guessCount = 0; // initial guess count
+            int guessLimit = 3; // guess limit of 4 chances to guess secret number since initial count is 0
+            bool outOfGuesses = false; // boolean indicating if user is out of guesses
 
-            if (parsedGuess == secretNum)
+            // while user's guess does not match secret number and user is not out of guesses, keep looping/asking user to guess
+            while (parsedGuess != secretNum && !outOfGuesses)
             {
-                Console.WriteLine(@"
+                if (guessCount < guessLimit) // if user has not reach guess limit, execute code below:
+                {
+                    Console.WriteLine(@" 
                       _...._
                     .`      `.
                    / ***      \         
                   : **         :         
-                  :            :    Yay, you got it right!                             
-                   \          /       
+                  :            :   Aww, you guessed incorrectly!                             
+                   \          /    Please try again! 
                     `-.,,,,.-'              
                      _(    )_
                     )        (
                    (          )
                     `-......-`  
+                    ");
+
+                    Console.Write("Enter your guess: "); // prompt user to guess number again
+                    parsedGuess = int.Parse(Console.ReadLine()); // converting string to int and storing user's guess to variable
+                    guessCount++; // increment the guess count for each guess
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+
+            if (outOfGuesses)
+            {
+                Console.WriteLine(@"
+                    _...._
+                  .`      `.
+                 / ***      \         
+                : **         :    You guessed incorrectly!     
+                :            :    Sorry, you are out of guesses!                             
+                 \          /       
+                  `-.,,,,.-'              
+                   _(    )_
+                  )        (
+                 (          )
+                  `-......-`  
                 ");
             }
             else
             {
                 Console.WriteLine(@"
-                        
-                      _...._
-                    .`      `.
-                   / ***      \         
-                  : **         :         
-                  :            :    Aww, you guessed incorrectly!                             
-                   \          /       
-                    `-.,,,,.-'              
-                     _(    )_
-                    )        (
-                   (          )
-                    `-......-` 
+                    _...._
+                  .`      `.
+                 / ***      \         
+                : **         :         
+                :            :    Yay, you got it right!                             
+                 \          /       
+                  `-.,,,,.-'              
+                   _(    )_
+                  )        (
+                 (          )
+                  `-......-`  
                 ");
             }
-
         }
     }
 }
